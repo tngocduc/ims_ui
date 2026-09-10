f36d154294fea4a17ffe9bd341a53151b9fe4138
Duc Tran <tngocduc@gmail.com>
2026-09-10 11:37:49 +0700

    Adding allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    allowedHosts: ['vend.traninc.vn'],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
